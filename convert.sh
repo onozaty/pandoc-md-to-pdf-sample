4e5f52d5da5754005f7b56fdb6adf7a5c090ad10
onozaty <onozaty@gmail.com>
2026-06-24 23:44:44 +0000

    chore: Dockerイメージのバージョンを1.0.1に固定し変数化

--- convert.sh
+++ convert.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
+IMAGE=ghcr.io/onozaty/pandoc-mermaid-ja:1.0.1
+
 mkdir -p output
 
 for md in docs/*.md; do
   filename=$(basename "$md" .md)
   echo "Converting $md -> output/${filename}.pdf"
-  docker run --rm -v "$PWD":/data ghcr.io/onozaty/pandoc-mermaid-ja "$md" -o "output/${filename}.pdf"
+  docker run --rm -v "$PWD":/data "$IMAGE" "$md" -o "output/${filename}.pdf"
 done
 
 echo "Done."
